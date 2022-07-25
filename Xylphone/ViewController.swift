078ae9d0235597e5822a99286607d39396612132
Mohanad Abu Sabha <mohannad.naxol@gmail.com>
2022-07-26 02:44:50 +0300

    code updated

--- Xylphone/ViewController.swift
+++ Xylphone/ViewController.swift
@@ -11,16 +11,22 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
+    
     @IBAction func keyPressed(_ sender: UIButton) {
         playSound(button: sender.currentTitle!)
+        
+        //Reduces the sender's opacity to half.
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
     }
     
     func playSound(button: String) {
+        // finding the sound file in our app bundle
         let url = Bundle.main.url(forResource: button, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
